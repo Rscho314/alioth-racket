d0011da5a650d07e542f1927a5cb92a831d99d9e
Raoul Schorer <raoul@desktop-pc.home>
2020-10-29 22:54:24 +0100

    apparently correct reverse-complement

--- reverse-complement/places.rkt
+++ reverse-complement/places.rkt
@@ -1,35 +1,19 @@
 #lang racket/base
 
-;;; TODO:
-;;; Add newlines (line length 60)
-;;; parallelize
-
-(require racket/place
+(require ;racket/place
          (only-in racket/fixnum
                   fxvector-set!
                   fxvector-ref
                   make-fxvector)
-         racket/list
-         racket/bytes
-         racket/port
-         ;optimization-coach
-         ;racket/performance-hint
+         (only-in racket/list flatten)
          ;racket/require
          ;(for-syntax racket/base)
-         #;(filtered-in (lambda (name) (regexp-replace #rx"unsafe-" name ""))
-                      racket/unsafe/ops)
-         #;(rename-in racket/unsafe/ops
-                    [unsafe-fx+ +]
-                    [unsafe-fx+ -]
-                    [unsafe-fx* *]
-                    [unsafe-fxquotient quotient]
-                    [unsafe-char->integer char->integer]
-                    [unsafe-fxvector-set! fxvector-set!]
-                    [unsafe-fxvector-ref fxvector-ref]
-                    [unsafe-vector-set! vector-set!]
-                    [unsafe-bytes-length bytes-length]))
+         ;(filtered-in (lambda (name) (regexp-replace #rx"unsafe-" name "")) racket/unsafe/ops)
+         )
 
-(define lookup-table (make-fxvector 128 46))
+(define line-length 60)
+(define LF (char->integer #\newline))
+(define lookup-table (make-fxvector 128))
 (define buffer-size (* 64 1024))
 
 (for ([from (in-string "ACGTUMRWSYKVHDBN")]
@@ -40,39 +24,44 @@
 
 (define (reverse-complement fasta-bytes lookup-table)
   (define output-size (bytes-length fasta-bytes))
-  (define indices (flatten (list 0 (regexp-match-positions* #rx">[^\n]+\n" fasta-bytes) output-size)))
-  (define seq-bytes (make-bytes output-size 95))
+  (define seq-bytes (make-bytes output-size))
   (letrec
       ([sequences (lambda (i c)
-                    (begin
-                      ;; INDICES APPEAR OK, WORK ON BYTE PERMUTATION!
-                      (define start (car i))
-                      (define end (cadr i))
-                      (for ([n (in-range (- end start))])
-                        (define dec-index (- end n 1))
-                        (define inc-index (+ n start))
-                        (define dec-byte (bytes-ref fasta-bytes dec-index))
-                        (cond
-                          #;[(= 10 dec-byte) (begin
-                                             (bytes-set! seq-bytes dec-index 10)
-                                             #;(bytes-set! seq-bytes inc-index 33))]
-                          [else (bytes-set! seq-bytes
-                                    inc-index
-                                    (fxvector-ref lookup-table dec-byte))]))
-                      (heads (cdr i) (+ 1 c))))]
+                    (define start (car i))
+                    (define end (cadr i))
+                    (define write-offset 0)
+                    (define current-char 0)
+                    (for ([n (in-range start end)])
+                      (define write-index (- n write-offset))
+                      (define src-byte (bytes-ref fasta-bytes (- end (- n start) 1)))
+                      (cond
+                        [(= LF src-byte) (begin (set! write-offset (+ write-offset 1)))]
+                        [(= line-length current-char)
+                         (begin (bytes-set! seq-bytes write-index LF)
+                                (bytes-set! seq-bytes (+ write-index 1) (fxvector-ref lookup-table src-byte))
+                                (set! write-offset (- write-offset 1))
+                                (set! current-char 1))]
+                        [(= write-index (- end 2)) (begin
+                                                     (bytes-set! seq-bytes write-index (fxvector-ref lookup-table src-byte))
+                                                     (bytes-set! seq-bytes (+ 1 write-index) LF))]
+                        [(not (= LF src-byte)) (begin
+                                                 (bytes-set! seq-bytes write-index (fxvector-ref lookup-table src-byte))
+                                                 (set! current-char (+ current-char 1)))]))
+                    (heads (cdr i) (+ 1 c)))]
        [heads (lambda (i c)
                 (cond
                   [(null? (cdr i)) seq-bytes]
                   [else (begin
                           (bytes-copy! seq-bytes (car i) fasta-bytes (car i) (cadr i))
                           (sequences (cdr i) (+ 1 c)))]))])
-    (sequences indices 0)))
+    (sequences (flatten (list 0 (regexp-match-positions* #rx">[^\n]+\n" fasta-bytes) output-size)) 0)))
 
+;(define (main)
   (let loop ([input-buffer (read-bytes buffer-size)])
              (if (eof-object? input-buffer)
                  (void)
                  (begin (write-bytes (reverse-complement input-buffer lookup-table))
                         (loop (read-bytes buffer-size)))))
-
-
-;(with-input-from-file "revcomp-input.txt" (lambda () (write-bytes (port->bytes))))
+;)
+;(require racket/port)
+;(with-input-from-file "revcomp-input.txt" (lambda () (main)))
